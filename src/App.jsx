994c337fee3e1154850696450aba63466576b529
Asmit <140798529+iAsmit19@users.noreply.github.com>
2024-08-28 02:17:42 +0530

    Navigation_Update-0.2
    - Added the Navigation Bar
    - Added the Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import "./index.module.css";
+import { Route, Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { mainContainer } from "./stylesheets/app.module.css";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 const App = () => {
-  return <></>;
+  return (
+    <div className={mainContainer}>
+      {/* <Router> */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      {/* </Router> */}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+import { cartContainer } from "../stylesheets/cart.module.css";
+
+const Cart = () => {
+  const vid_time = 18.23;
+  console.log(vid_time);
+  
+  return (
+    <div className={cartContainer}>
+      Cart
+    </div>
+  );
+};
+
+export default Cart;
